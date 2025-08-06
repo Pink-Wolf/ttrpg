85958f7f5ea7c15bf72067b25c62560a513cbe91
Pink-Wolf <apple6and6tomato@gmail.com>
2025-08-06 08:55:48 +0200

    Minor improvements to skill page

--- TTRPGDocs/ttrpg.client/src/app/article/attributes+and+skills/page.tsx
+++ TTRPGDocs/ttrpg.client/src/app/article/attributes+and+skills/page.tsx
@@ -2,7 +2,7 @@ import "./page.module.css"
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Dice",
+    title: "Skills",
 };
 
 export default function SkillArticle() {
@@ -21,19 +21,19 @@ export default function SkillArticle() {
                 </thead>
                 <tbody>
                     <tr>
-                        <th>Physique</th>
+                        <th id="physique">Physique</th>
                         <td>The physical strength and constitution of a character&apos;s body.</td>
                     </tr>
                     <tr>
-                        <th>Wits</th>
+                        <th id="wits">Wits</th>
                         <td>The quick-thinking and dexterity of a character.</td>
                     </tr>
                     <tr>
-                        <th>Intellect</th>
+                        <th id="intellect">Intellect</th>
                         <td>The scholarly knowledge and reasoning of a character.</td>
                     </tr>
                     <tr>
-                        <th>Heart</th>
+                        <th id="heart">Heart</th>
                         <td>The charms, religious knowledge, and empathy of a character.</td>
                     </tr>
                 </tbody>
@@ -108,67 +108,67 @@ export default function SkillArticle() {
             <div id="skill-detail-section">
                 <section>
                     <h3>Physique</h3>
-                    <h4>Awe</h4>
+                    <h4 id="awe">Awe</h4>
                     The apparent strength of the character. This awe can inspire respect, hope in allies, and fear in opponents.
-                    <h4>Athletics</h4>
+                    <h4 id="athletics">Athletics</h4>
                     The ability to perform deeds requiring physical strength or a high stamina.
-                    <h4>Fortitude</h4>
+                    <h4 id="fortitude">Fortitude</h4>
                     The ability of the body to resist attacks, such as poisons, viruses, and alchohol.
-                    <h4>Body</h4>
+                    <h4 id="body">Body</h4>
                     Understanding of the body is primarily used when healing physical injuries.
-                    <h4>Awareness</h4>
+                    <h4 id="awareness">Awareness</h4>
                     A constant sense of what is going on, especially from the corner of one&apos;s eye. This awareness helps avoid surprises like an ambush or trap.
-                    <h4>Crafting</h4>
+                    <h4 id="crafting">Crafting</h4>
                     By hand or tool, crafting sculptures of stone or weapons of metal, this skill covers the arts requiring a strong hand, especially ones where function matters more than appearance.
                 </section>
 
                 <section>
                     <h3>Wits</h3>
-                    <h4>Trickery</h4>
+                    <h4 id="trickery">Trickery</h4>
                     The art of deception and manipulation, sharing only what is convenient for the other party to hear.
-                    <h4>Dodge</h4>
+                    <h4 id="dodge">Dodge</h4>
                     The ability to react and move out of the way of danger.
-                    <h4>Stealth</h4>
+                    <h4 id="stealth">Stealth</h4>
                     To remain hidden, or perform unseen actions, stealth keeps your presence and acts unknown to others.
-                    <h4>Nature</h4>
+                    <h4 id="nature">Nature</h4>
                     Knowledge of plants and animals, of what plants to combine to heal a burn, or where best to scratch a cat, you will hardly find someone who could not use more knowledge of nature.
-                    <h4>Find</h4>
+                    <h4 id="find">Find</h4>
                     Whether it be searching a room or following a trail, the ability to find both things of importance and things of profit is a prime skill for treasure hunters.
-                    <h4>Brewing</h4>
+                    <h4 id="brewing">Brewing</h4>
                     Whether cooking a healthy dinner, or combining some berries to make a deadly toxin, brewing is a natural skill often decried as witchcraft.
                 </section>
 
                 <section>
                     <h3>Intellect</h3>
-                    <h4>Reason</h4>
+                    <h4 id="reason">Reason</h4>
                     The ability to use logic to explain why you are always right.
-                    <h4>Warding</h4>
+                    <h4 id="warding">Warding</h4>
                     Perhaps more niche than other forms of protection, this skill resists pure magic and allows magical protection.
-                    <h4>Navigate</h4>
+                    <h4 id="navigate">Navigate</h4>
                     Whether by use of the stars or a map, the ability to locate your current position and orientation.
-                    <h4>Lore</h4>
+                    <h4 id="lore">Lore</h4>
                     While some may be skilled in specific fields, this skill covers a general and random collection of knowledge spanning history and the fundamental nature of the world.
-                    <h4>Study</h4>
+                    <h4 id="study">Study</h4>
                     Whether it be studying books for their knowledge, or experimenting on an object to learn of its function, this skill is invaluable for the scholarly folk.
-                    <h4>Arcane</h4>
+                    <h4 id="arcane">Arcane</h4>
                     The ability to cast spells and enchant tools, as well as understanding these spells and enchantments.
                 </section>
 
                 <section>
                     <h3>Heart</h3>
-                    <h4>Charm</h4>
+                    <h4 id="charm">Charm</h4>
                     Whether through words or just one&apos;s general likability,
                     the ability to control people&apos;s emotions or make them help you out of the goodness of their heart.
-                    <h4>Will</h4>
+                    <h4 id="will">Will</h4>
                     The strength of the mind to avoid magical and natural influences.
-                    <h4>Travel</h4>
+                    <h4 id="travel">Travel</h4>
                     Travelling for days with little going on is surprisingly hard.
                     To keep track of rations, to keep spirits high, to make sure noone falls behind, there is a lot to do which this skill helps with.
-                    <h4>People</h4>
+                    <h4 id="people">People</h4>
                     Whether it be the local customs, people&apos;s likely reaction, or the gods and their influence over communities, people are a complex field of study.
-                    <h4>Insight</h4>
+                    <h4 id="insight">Insight</h4>
                     Understanding the words unspoken, or the intentions hidden away, insight is the opponent of other&apos;s trickery, whether intentional or not.
-                    <h4>Storytelling</h4>
+                    <h4 id="storytelling">Storytelling</h4>
                     To craft stories, songs, myths and poems; to control people&apos;s emotion with nothing but words; it is hard to believe it is not arcane magic.
                 </section>
             </div>
