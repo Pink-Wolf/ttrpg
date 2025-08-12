b06b1c44459267910c08a238af258263299db42e
Pink-Wolf <apple6and6tomato@gmail.com>
2025-08-12 11:54:06 +0200

    Editors now disable while saving
    Editors now handles errors during saving.

--- TTRPGDocs/ttrpg.client/src/lib/editor/origin.tsx
+++ TTRPGDocs/ttrpg.client/src/lib/editor/origin.tsx
@@ -6,15 +6,22 @@ import { SubmitInput } from "@/Input"
 
 export default function OriginEditor({ initialValue, id }: { initialValue: Origin, id: string }) {
     const [value, setter] = useState<Origin>(initialValue)
+    const [processing, setProcessing] = useState<boolean>(false)
 
     function onSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
 
-        postOrigin(value)
+        setProcessing(true)
+        postOrigin(value).then(() => {
+            setProcessing(false)
+        }).catch(e => {
+            setProcessing(false)
+            alert(`Failed to save: ${e}`)
+        })
     }
 
     return (<form onSubmit={onSubmit}>
-        <OriginInput value={value} setter={setter} idPath={[id]} label="" />
+        <OriginInput value={value} setter={setter} idPath={[id]} label="" disabled={processing} />
         <SubmitInput label="Save" />
     </form>)
 }
