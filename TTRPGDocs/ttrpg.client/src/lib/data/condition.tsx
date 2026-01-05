b5ef028b5f9406c437e614f6205be0e7f1fc133a
Pink-Wolf <ponypencilpaper@gmail.com>
2026-01-05 21:01:56 +0100

    Wrote about physical wounds

--- TTRPGDocs/ttrpg.client/src/lib/data/condition.tsx
+++ TTRPGDocs/ttrpg.client/src/lib/data/condition.tsx
@@ -11,5 +11,10 @@ export function GetConditions(): Condition[] {
             summary: "Cannot act; caused by taking too much {Damage}.",
             description: "Cannot perform any actions.\n\nCharacters gain this condition when having more {Damage} than {Endurance}, and loses this condition when not."
         },
+        {
+            name: "Physical Wound",
+            summary: "Various negative conditions that requires rest to heal.",
+            description: "Different physical wounds provide different negative effects.\n\nPhysical Wounds are healed by a week of rest in a proper bed.\nIf it is unrealistic for mere rest to heal a particular wound, then proper treatment must first be acquired before the rest."
+        },
     ]
 }
